d8bb7012bf3cebc93b5b755ef1e0715479e5d0d2
summerinsect <127171251+summerinsect@users.noreply.github.com>
2025-10-16 09:26:39 +0800

    Finish Click Logic

--- Assets/Scripts/StageScripts/StageLoader.cs
+++ Assets/Scripts/StageScripts/StageLoader.cs
@@ -29,11 +29,11 @@ public class StageLoader : MonoBehaviour
 		BattleManager.instance.AddMember(1, CharacterCreater.instance.CreateCharacter("melee", 1, new Vector3Int(-2, 2, 0)));
 		BattleManager.instance.AddMember(1, CharacterCreater.instance.CreateCharacter("melee", 1, new Vector3Int(2, -3, 1)));
 		BattleManager.instance.AddMember(0, CharacterCreater.instance.CreateCharacter("melee", 0, new Vector3Int(-2, -3, 5)));
-        BagManager.instance.members.Add(CharacterCreater.instance.CreateCharacter("melee"));
         BagManager.instance.members.Add(CharacterCreater.instance.CreateCharacter("YouKnowWho"));
-        BagManager.instance.members.Add(CharacterCreater.instance.CreateCharacter("melee"));
         BagManager.instance.members.Add(CharacterCreater.instance.CreateCharacter("YouKnowWho"));
-        BagManager.instance.members.Add(CharacterCreater.instance.CreateCharacter("melee"));
+        BagManager.instance.members.Add(CharacterCreater.instance.CreateCharacter("YouKnowWho"));
+        BagManager.instance.members.Add(CharacterCreater.instance.CreateCharacter("YouKnowWho"));
+        BagManager.instance.members.Add(CharacterCreater.instance.CreateCharacter("YouKnowWho"));
     }
 
 }
